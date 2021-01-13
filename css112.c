3a3c639fd1b352e3abf6672353a79a938893650b
surawee <67056659+sura1542@users.noreply.github.com>
2021-01-13 15:37:57 +0700

    Add files via upload

---
+++ css112.c
@@ -0,0 +1,49 @@
+//A First progarm in c
+/*#include<stdio.h>
+int main()
+{
+    printf("Welcome to C! \n");
+    return 0;
+}*/
+
+#include<stdio.h>
+int main()
+/*{
+    int a,b,c;
+    a = 5;
+    b = 10;
+    c = a + b;
+    printf("c = %d\n",c);
+}*/
+
+/*{
+    int a,x,y,b,d;
+    a = scanf("%d\n",&a);
+    y = scanf("%d\n",&y);
+    b = scanf("%d\n",&b);
+    d = scanf("%d\n",&d);
+    x = (a*b) + (y*d);
+    x = (2*a-1)/5*y + 5*(y*y +1)/7*(b+1);
+    x = (2*a*b)-(d*d)/5*(a*a - 1) + 7*(a*a + b*b)/3*(d*d+2*d+1);
+    printf("x = %d\n",x);
+}*/
+/*   
+{    int a,b,c;
+    a = scanf("%.2f\n",&a);
+    b = scanf("%.2f\n",&b);
+    c = a*b+0.5;
+    printf(" c= %d\n");
+}*/
+
+{
+    float a,b,c;
+    /*int c;*/
+    printf("input a : ");
+    a = scanf("%f",&a);
+    printf("input b : ");
+    b = scanf("%f",&b);
+    c = (a * b) + 0.5;
+    printf("c = %d\n",c);
+}
+
+
