0943188afbe66702ad2c26fe3f77c3a293e46a45
surawee <67056659+sura1542@users.noreply.github.com>
2021-01-21 22:02:19 +0700

    Add files via upload

---
+++ kob.c
@@ -0,0 +1,12 @@
+#include <stdio.h>	 
+int num;	สร้างตัวแปรชื่อ num เพื่อเก็บข้อมูลชนิดจำนวนเต็ม
+float y;	สร้างตัวแปรชื่อ y เพื่อเก็บข้อมูลชนิดเลขทศนิยม
+char n;	สร้างตัวแปรชื่อ n เพื่อเก็บข้อมูลชนิดตัวอักขระ
+void main()	 
+{	 
+     printf("Enter number : ")	 
+     scanf("%d",&num);	 
+     printf("Enter name : ");	 
+     scanf("%f",&n);	 
+     printf("Thank you");	 
+}
