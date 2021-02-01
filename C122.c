7b2405e4df2d7adeae3d185294c59bf9d98c9d0a
surawee <67056659+sura1542@users.noreply.github.com>
2021-02-01 23:08:14 +0700

    Add files via upload

---
+++ C122.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+/*{
+    float a = 12000;
+    if (a > 10000)
+    {
+        a = a + (a*0.05);
+        printf(" a = %d\n",a);
+
+        else if (a < 10000)
+        {
+            a = a + (a*0.05);
+            printf(" a = %d\n",a);
+        }
+        
+    }
+    
+}
+
+{
+    float a,b,d;
+    int c;
+    printf("input a,b : ");
+    c = a*b;
+    d = a*b - c
+    if (d >= 5)
+    {
+        printf("c = %d\n",c);
+    }
+return(0);
+}*/
+
